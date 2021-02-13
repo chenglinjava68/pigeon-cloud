870729b230e457d41ff82c50db54de36ffa70e59
wangyidi0731 <wangyiidii@163.com>
2021-02-13 22:24:12 +0800

    业务常量类

--- pigeon-auth/src/main/java/cn/yiidii/pigeon/auth/AuthApplication.java
+++ pigeon-auth/src/main/java/cn/yiidii/pigeon/auth/AuthApplication.java
@@ -1,9 +1,11 @@
 package cn.yiidii.pigeon.auth;
 
+import cn.yiidii.pigeon.common.core.constant.BizConstants;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * 认证服务
@@ -11,9 +13,10 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
  * @author: YiiDii Wang
  * @create: 2021-02-12 00:16
  */
-@SpringBootApplication(scanBasePackages = {"cn.yiidii.pigeon"})
+@SpringBootApplication
 @EnableDiscoveryClient
-@EnableFeignClients(basePackages = {"cn.yiidii.pigeon.*.api"})
+@ComponentScan({BizConstants.BUSINESS_PACKAGE})
+@EnableFeignClients(value = BizConstants.BUSINESS_PACKAGE)
 public class AuthApplication {
     public static void main(String[] args) {
         SpringApplication.run(AuthApplication.class, args);
