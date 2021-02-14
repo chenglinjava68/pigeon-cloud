16ce568f693987b9078f30003e8a6b446e339df2
wangyidi0731 <wangyiidii@163.com>
2021-02-15 00:17:01 +0800

    core模块集成redis相关操作

--- pigeon-demo/demo-biz/src/main/java/cn/yiidii/pigeon/demo/controller/TestDemoController.java
+++ pigeon-demo/demo-biz/src/main/java/cn/yiidii/pigeon/demo/controller/TestDemoController.java
@@ -1,7 +1,7 @@
 package cn.yiidii.pigeon.demo.controller;
 
 import cn.yiidii.pigeon.common.core.base.R;
-import cn.yiidii.pigeon.common.core.exception.BizException;
+import cn.yiidii.pigeon.common.core.redis.RedisOps;
 import cn.yiidii.pigeon.rbac.api.dto.UserDTO;
 import cn.yiidii.pigeon.rbac.api.feign.UserFeign;
 import com.alibaba.fastjson.JSONObject;
@@ -9,6 +9,9 @@ import lombok.extern.slf4j.Slf4j;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author: YiiDii Wang
@@ -21,13 +24,25 @@ public class TestDemoController {
 
     private final UserFeign userFeign;
 
+    private final RedisOps redisOps;
+
     @GetMapping("/test/hello")
     public R<String> hello() {
         return R.ok(null, "hello demo");
     }
 
+    @GetMapping("/test/redis")
+    public R<String> redis(@RequestParam String key, @RequestParam String value) {
+        redisOps.set(key, value);
+        Map<String, String> map = new HashMap<>();
+        map.put(key, value);
+        redisOps.putHashValues("hash", map);
+        return R.ok(null, JSONObject.toJSONString(map));
+    }
+
     /**
      * 需要登陆，但不需要权限
+     *
      * @return
      */
     @GetMapping("aaa")
@@ -37,6 +52,7 @@ public class TestDemoController {
 
     /**
      * 需要登陆，但不需要权限
+     *
      * @return
      */
     @GetMapping("bbb")
@@ -47,6 +63,7 @@ public class TestDemoController {
 
     /**
      * 需要登陆，且需要user权限
+     *
      * @param username
      * @return
      */
@@ -57,8 +74,6 @@ public class TestDemoController {
         log.info("test user: " + JSONObject.toJSON(userDTOResp));
         return userDTOResp;
     }
-
-
 
 
 }
