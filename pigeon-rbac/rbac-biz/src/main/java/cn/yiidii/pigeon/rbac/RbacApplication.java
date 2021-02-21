185c6b884fb1b8a8df2383ca20c98cb53bf98c64
wangyidi0731 <wangyiidii@163.com>
2021-02-21 16:22:07 +0800

    组件扫描，feign

--- pigeon-rbac/rbac-biz/src/main/java/cn/yiidii/pigeon/rbac/RbacApplication.java
+++ pigeon-rbac/rbac-biz/src/main/java/cn/yiidii/pigeon/rbac/RbacApplication.java
@@ -1,10 +1,13 @@
 package cn.yiidii.pigeon.rbac;
 
+import cn.yiidii.pigeon.common.core.constant.BizConstants;
 import cn.yiidii.pigeon.common.knife4j.annotation.EnablePigeonKnife4j;
 import cn.yiidii.pigeon.common.security.annotation.EnableResource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * rbac 启动类
@@ -15,6 +18,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnablePigeonKnife4j
+@ComponentScan({BizConstants.BUSINESS_PACKAGE})
+@EnableFeignClients(value = BizConstants.BUSINESS_PACKAGE)
 @EnableResource
 public class RbacApplication {
 
