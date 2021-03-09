b9680c6dabd012d8b1a060034d1f610c15026e91
wangyidi0731 <wangyiidii@163.com>
2021-03-09 23:46:19 +0800

    附件mapper

---
+++ pigeon-file/file-biz/src/main/java/cn/yiidii/pigeon/file/mapper/AttachmentMapper.java
@@ -0,0 +1,13 @@
+package cn.yiidii.pigeon.file.mapper;
+
+import cn.yiidii.pigeon.file.api.entity.Attachment;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @author: YiiDii Wang
+ * @create: 2021-03-09 23:20
+ */
+@Mapper
+public interface AttachmentMapper extends BaseMapper<Attachment> {
+}
