fb982e9406b53ab251d4c069b30242fdfe06dfaa
wangyidi0731 <wangyiidii@163.com>
2021-03-14 18:14:09 +0800

    附件接口

---
+++ pigeon-file/file-biz/src/main/java/cn/yiidii/pigeon/file/controller/FileController.java
@@ -0,0 +1,50 @@
+package cn.yiidii.pigeon.file.controller;
+
+import cn.yiidii.pigeon.common.core.base.R;
+import cn.yiidii.pigeon.file.api.entity.Attachment;
+import cn.yiidii.pigeon.file.properties.OssProperties;
+import cn.yiidii.pigeon.file.service.IAttachmentService;
+import com.alibaba.fastjson.JSONObject;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+/**
+ * 文件接口
+ *
+ * @author: YiiDii Wang
+ * @create: 2021-03-09 23:15
+ */
+@Slf4j
+@RestController
+@RequestMapping("attachment")
+@Api(tags = "文件接口")
+@RequiredArgsConstructor
+public class FileController {
+
+    private final IAttachmentService attachmentService;
+    private final OssProperties ossProperties;
+
+    @PostMapping("/upload")
+    @ApiOperation(value = "文件上传")
+    public R<Attachment> uploadFile(@RequestParam(value = "file") MultipartFile file) {
+        Attachment attachment = attachmentService.upload(file);
+        return R.ok(attachment, "上传成功");
+    }
+
+    @GetMapping("/list")
+    @ApiOperation(value = "文件列表")
+    public R attachment() {
+        List<Attachment> attachmentList = attachmentService.lambdaQuery().list();
+        JSONObject jo = new JSONObject();
+        jo.put("customDomain", ossProperties.getCustomDomain());
+        jo.put("record", attachmentList);
+        return R.ok(jo);
+    }
+
+}
