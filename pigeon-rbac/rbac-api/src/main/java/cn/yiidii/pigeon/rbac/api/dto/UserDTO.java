4285d28112963aeb7583295f6b6825b2161c9e66
wangyidi0731 <wangyiidii@163.com>
2021-03-14 13:57:47 +0800

    确认密码字段

--- pigeon-rbac/rbac-api/src/main/java/cn/yiidii/pigeon/rbac/api/dto/UserDTO.java
+++ pigeon-rbac/rbac-api/src/main/java/cn/yiidii/pigeon/rbac/api/dto/UserDTO.java
@@ -5,6 +5,7 @@ import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import io.swagger.annotations.ApiModel;
 
+import javax.validation.constraints.NotBlank;
 import java.util.List;
 
 /**
@@ -14,6 +15,10 @@ import java.util.List;
 @Data
 @ApiModel("用户DTO")
 public class UserDTO extends User {
+
+    @ApiModelProperty(value = "确认密码")
+    @NotBlank(message = "确认密码不能为空",groups = {Update.class})
+    private String confirmPassword;
 
     @ApiModelProperty(value = "角色")
     private List<RoleDTO> roles;
