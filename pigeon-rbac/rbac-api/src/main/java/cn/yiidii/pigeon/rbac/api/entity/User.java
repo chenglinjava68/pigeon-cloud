ddf7adc53138433de346bec789bcfa54c3fa3e16
wangyidi0731 <wangyiidii@163.com>
2021-03-14 13:55:34 +0800

    搜索参数类

--- pigeon-rbac/rbac-api/src/main/java/cn/yiidii/pigeon/rbac/api/entity/User.java
+++ pigeon-rbac/rbac-api/src/main/java/cn/yiidii/pigeon/rbac/api/entity/User.java
@@ -9,6 +9,7 @@ import lombok.experimental.Accessors;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import java.time.LocalDateTime;
 
 /**
@@ -29,20 +30,35 @@ public class User extends Entity<Long> {
 
     @NotBlank(message = "用户名不能为空")
     private String username;
+
+    @NotBlank(message = "密码不能为空", groups = {Add.class, Update.class})
     @TableField(value = "password")
     private String password;
+
     @TableField(value = "salt")
     private String salt;
+
     @TableField(value = "name")
     private String name;
+
+    @Pattern(regexp = "^\\s*\\w+(?:\\.{0,1}[\\w-]+)*@[a-zA-Z0-9]+(?:[-.][a-zA-Z0-9]+)*\\.[a-zA-Z]+\\s*$", message = "邮箱格式不正确")
     private String email;
+
+    @Pattern(regexp = "^((13[0-9])|(14[0,1,4-9])|(15[0-3,5-9])|(16[2,5,6,7])|(17[0-8])|(18[0-9])|(19[0-3,5-9]))\\d{8}$", message = "手机号格式不正确")
     private String mobile;
+
+    @Pattern(regexp = "^[12]$", message = "性别输入错误")
     private String sex;
+
     private String avatar;
+
     @TableField(value = "`desc`")
     private String desc;
+
     private Integer pwdErrTimes;
+
     private LocalDateTime lastPwdErrTime;
+
     private LocalDateTime lastLoginTime;
 
 }
