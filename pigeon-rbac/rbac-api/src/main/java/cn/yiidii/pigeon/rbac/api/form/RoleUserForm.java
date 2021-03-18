ddb70e416efb27d92ddb34abfe4eca178a03e2fa
wangyidi0731 <wangyiidii@163.com>
2021-03-18 18:25:29 +0800

    去掉注释

--- pigeon-rbac/rbac-api/src/main/java/cn/yiidii/pigeon/rbac/api/form/RoleUserForm.java
+++ pigeon-rbac/rbac-api/src/main/java/cn/yiidii/pigeon/rbac/api/form/RoleUserForm.java
@@ -6,7 +6,6 @@ import lombok.Data;
 
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
 import java.util.List;
 
 /**
