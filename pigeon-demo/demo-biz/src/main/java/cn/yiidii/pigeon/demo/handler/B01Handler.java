5edca095e952a8975edd81172e034f65fb3f2ef5
wangyidi0731 <wangyiidii@163.com>
2021-06-07 10:14:09 +0800

    策略业务组件扫描classpath参数化

--- pigeon-demo/demo-biz/src/main/java/cn/yiidii/pigeon/demo/handler/B01Handler.java
+++ pigeon-demo/demo-biz/src/main/java/cn/yiidii/pigeon/demo/handler/B01Handler.java
@@ -3,14 +3,13 @@ package cn.yiidii.pigeon.demo.handler;
 import cn.yiidii.pigeon.common.strategy.annotation.HandlerType;
 import cn.yiidii.pigeon.common.strategy.handler.AbstractHandler;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 
 /**
  * @author YiiDii Wang
  * @create 2021-06-06 13:30
  */
 @Component("B01Handler")
-@HandlerType(bizCode = "A01", beanName = "B01Handler")
+@HandlerType(bizCode = "B01", beanName = "B01Handler")
 public class B01Handler implements AbstractHandler {
 
     @Override
